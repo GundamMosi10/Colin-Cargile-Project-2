f026f5cfb5e0ca0af6a61869dd177b3b9d507e85
Colin Mosi Cargile <cargile10@gmail.com>
2022-11-09 14:36:42 -0500

    created the mucicstore recommendations project and added it to the main project folder

---
+++ M7-Challenge-Cargile-Colin/musicstore-recommendations/src/main/java/com/company/musicstorerecommendations/MusicstorerecommendationsApplication.java
@@ -0,0 +1,13 @@
+package com.company.musicstorerecommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstorerecommendationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicstorerecommendationsApplication.class, args);
+	}
+
+}
